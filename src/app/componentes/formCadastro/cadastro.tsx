fc523292802f9523eff73c53a3ff5a9701ed4587
LeonanAraudo <146787577+LeonanAraudo@users.noreply.github.com>
2024-11-15 14:38:49 -0300

    first commit

---
+++ src/app/componentes/formCadastro/cadastro.tsx
@@ -0,0 +1,79 @@
+'use client'
+import Box from '@mui/material/Box';
+import * as React from 'react';
+import IconButton from '@mui/material/IconButton';
+import Input from '@mui/material/Input';
+import InputLabel from '@mui/material/InputLabel';
+import InputAdornment from '@mui/material/InputAdornment';
+import FormControl from '@mui/material/FormControl';
+import TextField from '@mui/material/TextField';
+import Visibility from '@mui/icons-material/Visibility';
+import VisibilityOff from '@mui/icons-material/VisibilityOff'
+
+export default function Cadastro(){
+    const [showPassword, setShowPassword] = React.useState(false);
+
+    const handleClickShowPassword = () => setShowPassword((show) => !show);
+  
+    const handleMouseDownPassword = (event: React.MouseEvent<HTMLButtonElement>) => {
+      event.preventDefault();
+    };
+  
+    const handleMouseUpPassword = (event: React.MouseEvent<HTMLButtonElement>) => {
+      event.preventDefault();
+    };
+    return(
+    <>
+    <p>Login</p>
+             <Box
+                component="form"
+                sx={{ m: 1, width: '35ch',display:'flex',flexDirection:'column' } }
+                >
+                <TextField id="standard-basic" label="Nome" variant="standard" />
+                <FormControl sx={{ width: '35ch' }} variant="standard">
+                    <InputLabel htmlFor="standard-adornment-password">Senha</InputLabel>
+                    <Input
+                        id="standard-adornment-password"
+                        type={showPassword ? 'text' : 'password'}
+                        endAdornment={
+                        <InputAdornment position="end">
+                            <IconButton
+                            aria-label={
+                                showPassword ? 'hide the password' : 'display the password'
+                            }
+                            onClick={handleClickShowPassword}
+                            onMouseDown={handleMouseDownPassword}
+                            onMouseUp={handleMouseUpPassword}
+                            >
+                            {showPassword ? <VisibilityOff /> : <Visibility />}
+                            </IconButton>
+                        </InputAdornment>
+                        }
+                    />
+                </FormControl>
+                <FormControl sx={{ width: '35ch' }} variant="standard">
+                    <InputLabel htmlFor="standard-adornment-password">Confirmar Senha</InputLabel>
+                    <Input
+                        id="standard-adornment-password"
+                        type={showPassword ? 'text' : 'password'}
+                        endAdornment={
+                        <InputAdornment position="end">
+                            <IconButton
+                            aria-label={
+                                showPassword ? 'hide the password' : 'display the password'
+                            }
+                            onClick={handleClickShowPassword}
+                            onMouseDown={handleMouseDownPassword}
+                            onMouseUp={handleMouseUpPassword}
+                            >
+                            {showPassword ? <VisibilityOff /> : <Visibility />}
+                            </IconButton>
+                        </InputAdornment>
+                        }
+                    />
+                </FormControl>
+                <button>Cadastrar</button>
+             </Box>
+    </>
+    )
+}
